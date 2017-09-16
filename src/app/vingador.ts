50fcee1d0129f7104ed27afb5ae3cd8966024472
Mari <silvamariabrito20@gmail.com>
2017-09-16 20:12:39 -0300

    usando cards

--- src/app/vingador.ts
+++ src/app/vingador.ts
@@ -2,10 +2,14 @@ export class Vingador {
     id: number;
     nome: string;
     pessoa: string;
+    urlFoto: string;
+    
 
-    constructor(id: number, nome: string, pessoa?: string) {
+    constructor(id: number, nome: string, pessoa: string, urlFoto?:string) 
+    {
         this.id = id;
         this.nome = nome;
         this.pessoa = pessoa;
+        this.urlFoto= urlFoto;
     }
 }
